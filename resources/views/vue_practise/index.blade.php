6ee2531f05b7dfd881a84ddf84ecd605015188cd
joyanta1000 <joyanta955@gmail.com>
2022-04-26 11:36:19 +0600

    init

---
+++ resources/views/vue_practise/index.blade.php
@@ -0,0 +1,6 @@
+@extends('vue_practise.layouts.master')
+@section('content')
+<div id="app">
+    <example-component></example-component>
+</div>
+@endsection
---
+++ resources/views/vue_practise/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('vue_practise.layouts.includes.head')
+</head>
+
+<body>
+    @yield('content')
+    @include('vue_practise.layouts.includes.scripts')
+</body>
+
+</html>
